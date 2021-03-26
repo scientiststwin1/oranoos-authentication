b2b39ed48256a98221a531d35547ae92b562127c
mohammad abbasi <scientists.twin@gmail.com>
2021-03-26 13:05:59 +0430

    feautre: add repositories to module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './repository/user.repository';
+import { RoleRepository } from './repository/role.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      UserRepository,
+      RoleRepository
+    ])
+  ],
   providers: [UserService],
   controllers: [UserController]
 })
