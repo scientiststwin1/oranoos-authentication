2f472adaa8c46a14f0c246b7bb13a3f4f4c2f75c
mohammad abbasi <scientists.twin@gmail.com>
2021-03-26 10:42:40 +0430

    feautre: add swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Authentication')
+    .setDescription('oranoos authentication')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
